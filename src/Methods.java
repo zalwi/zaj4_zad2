67d2411fea359daca614c47dce89f008802f1a51
Kamil Zalwert <kamilz89@gmail.com>
2020-04-17 16:11:02 +0200

    zaj4_zad2; konstruktor, gettery i settery

--- src/Methods.java
+++ src/Methods.java
@@ -1,6 +1,6 @@
 class Methods {
     void changeTempAndShowStatus(Room room){
-        System.out.println("Aktualna temperatura: " + room.temperature + "C");
+        System.out.println("Aktualna temperatura: " + room.getTemperature() + "C");
         System.out.println("Próba obniżenia temperatury: " + room.decreaseTemperature());
     }
 }
--- src/Room.java
+++ src/Room.java
@@ -1,8 +1,11 @@
 class Room {
-    double area;
-    double temperature;
-    boolean airConditioning;
+    private double area;
+    private double temperature;
+    private boolean airConditioning;
     private final double limitOfTemperature = 15;
+
+    public Room() {
+    }
 
     public Room(double area, double temperature, boolean airConditioning) {
         this.area = area;
@@ -27,5 +30,33 @@ class Room {
             return true;
         }
 
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public boolean isAirConditioning() {
+        return airConditioning;
+    }
+
+    public void setAirConditioning(boolean airConditioning) {
+        this.airConditioning = airConditioning;
+    }
+
+    public double getLimitOfTemperature() {
+        return limitOfTemperature;
     }
 }
